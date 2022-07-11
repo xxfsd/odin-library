d86d83fde1c2444916539f1cd48ba59575d90f0a
xxfsd <josh.fsd@protonmail.com>
2022-07-11 22:04:09 +1000

    Created library array with 3x placeholder books, created class object for books

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "American Psycho",
+        author: "Bret Easton Ellis",
+        pages: 399
+    },
+    {
+        title: "The Shining",
+        author: "Stephen King",
+        pages: 477
+    },
+    {
+        title: "I, Robot",
+        author: "Isaac Asimov",
+        pages: 253
+    }
+];
+
+class Book {
+    constructor(title, author, pages){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}
